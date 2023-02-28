d4d9972fb686211c92197ec69eece95293670d47
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2023-02-27 21:00:28 -0300

    chore(*): add value string

--- src/Exceptions/ArrayParsingException.ts
+++ src/Exceptions/ArrayParsingException.ts
@@ -1,5 +1,5 @@
 export class ArrayParsingException extends Error {
 	constructor (value: string) {
-		super(`Failed to turn "${value}" into an array.`)
+		super(`Failed to turn "${JSON.stringify(value)} ${value.slice(0, Math.floor(value.length / 2))}" into an array.`)
 	}
 }
